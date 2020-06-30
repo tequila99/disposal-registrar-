10371612a1935dcb60ca55d1199c31e1fcb35924
Aleksey <akobzev@medsoft.su>
2020-06-30 15:46:59 +0300

    Изменения для публикации как npm-пакета
    - Добавлена обработка ошибки SCardGetStatusChange(), возникающей при быстром вкл-откл usb устрйств
    - Добавлена возможность через аргументы ф-и: задавать порт, не использовать сканер\картридер

--- src/index.js
+++ src/index.js
@@ -3,16 +3,36 @@ import socketio from 'socket.io'
 import socket from './socket'
 import { createServer } from 'http'
 
-const PORT = process.env.PORT || 3030
 const server = createServer(app)
 const io = socketio(server)
 
-app.set('port', PORT)
+const defaultOptions = {
+  port: process.env.PORT || 3030,
+  cardReader: true,
+  scanner: true
+}
 
-socket(io)
+// app.set('port', PORT)
 
-server.listen(PORT, () => {
-  console.log('*********************************************************************')
-  console.log(`Сервис для работы с локальным оборудованием запущен на порту: ${PORT}`)
-  console.log('*********************************************************************')
-})
+/**
+ *
+ * @param {{port: number, cardReader: boolean, scanner: boolean}} options
+ */
+const periphery = (options = defaultOptions) => {
+  const { port = defaultOptions.port, cardReader = defaultOptions.cardReader, scanner = defaultOptions.scanner } = options
+  socket(io, cardReader, scanner)
+  app.set('port', port)
+  server.listen(port, () => {
+    console.log('*********************************************************************')
+    console.log(`Сервис для работы с локальным оборудованием запущен на порту: ${port}`)
+    console.log('*********************************************************************')
+  })
+}
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
+// this module was run directly from the command line as in node xxx.js
+// require.main === module && NODE_ENV === 'development' && periphery({ port: 3035, cardReader: false, scanner: true })
+require.main === module && NODE_ENV === 'development' && periphery()
+
+module.exports = periphery
